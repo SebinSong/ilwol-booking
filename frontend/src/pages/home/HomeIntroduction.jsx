d93bf6ab637a92eba6f61b54351009c830e7729d
SebinSong <songxevin@gmail.com>
2024-01-02 18:32:56 +0900

    add youtube video player

--- frontend/src/pages/home/HomeIntroduction.jsx
+++ frontend/src/pages/home/HomeIntroduction.jsx
@@ -47,17 +47,27 @@ export default function HomeIntroduction ({
 
                   <u1 className='intro-list mb-50'>
                     <li className='intro-list-item'>
-                      <span className='has-text-bold'>20년차</span> 무당으로, 맑고 고운 영의 소유자 십니다.
+                      <span className='has-text-bold'>20년</span> 경력의 맑고 고운 영의 소유자십니다.
                     </li>
                     <li className='intro-list-item'>천신 도사/선녀님을 메인 신령님으로 모시고 계십니다.</li>
-                    <li className='intro-list-item'><span className='has-text-bold'>미래 지향적</span> 방향의 상담을 추구하시고, <span className='has-text-bold'>Fact</span>를 잘 맞추십니다.</li>
-                    <li className='intro-list-item'>하관/낙서/주역/명리학을 천상에서 공부하신 신의 영매시며, 사심없이 진리에 충실한 제자이십니다.</li>
+                    <li className='intro-list-item'><span className='has-text-bold'>미래 지향적</span> 상담을 추구하십니다. (심리 영혼치유의 카운셀링 기법입니다)</li>
+                    <li className='intro-list-item'>하관 / 낙서 / 주역 / 명리학 / 기문둔갑을 천상에서 공부하신 신의 영매시며, 사심없이 진리와 이치에 충실한 제자이십니다.</li>
                   </u1>
 
                   <h3 className='home-section-title mb-30'>
                     <span className='icon-tag is-prefix'></span>
                     <span className='text'>JTBC 뉴스 소개</span>
                   </h3>
+
+                  <div className='youtube-player-container'>
+                    <iframe width='560' height='291'
+                      title='Youtube video player'
+                      frameborder="0"
+                      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                      src="https://www.youtube-nocookie.com/embed/aGHIsmhuPn8"
+                      allowFullScreen>
+                    </iframe>
+                  </div>
                 </div>
               </div>
             </>

54d2366e795f6dc5434d47ea3725ba1e27fac126
SebinSong <songxevin@gmail.com>
2024-01-07 14:23:14 +0900

    implement syncing the google calendar

--- backend/routes/configRoutes.js
+++ backend/routes/configRoutes.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const { getDayoffs } = require('../controllers/dayoffControllers')
+const { clearCalendar, regenateAllEvents } = require('../controllers/calendarControllers')
 const router = express.Router()
 
 router.get('/dayoffs', getDayoffs)
+
+// google calendar related jobs
+router.delete('/calendar', clearCalendar)
+router.post('/calendar/all', regenateAllEvents)
 
 module.exports = router

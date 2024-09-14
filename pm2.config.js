d9bfa966ab745bb52914534b92d8a0de6383d988
SebinSong <songxevin@gmail.com>
2024-09-14 17:09:30 +1200

    env prod

--- pm2.config.js
+++ pm2.config.js
@@ -4,6 +4,7 @@ module.exports = {
     script : "./backend/server.js",
     instances: 0,
     out_file: "/dev/null",
-    error_file: "/dev/null"
+    error_file: "/dev/null",
+    log_file: "/dev/null"
   }]
 }
